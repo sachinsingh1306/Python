5a932db96db7ae5b833bf5012191bb832cb0d196
Sachin Singh <sachin8174056@gmail.com>
2025-12-13 10:26:29 +0530

    update

--- Chapter 7/pattern.py
+++ Chapter 7/pattern.py
@@ -1,17 +1,40 @@
-n = int(input("Enter a  number..."))
+# n = int(input("Enter a  number..."))
 
-def pattern(n):
-    if n == 0:
-        return
-    print("*" * n)
-    pattern(n-1)
+# def pattern(n):
+#     if n == 0:
+#         return
+#     print("*" * n)
+#     pattern(n-1)
 
-pattern(n)
+# pattern(n)
 
-# Convert inches to cm
-inch = int(input("Enter an inch: "))
+# # Convert inches to cm
+# inch = int(input("Enter an inch: "))
 
-def inch_to_cm(inch_value):
-    return inch_value * 2.54
+# def inch_to_cm(inch_value):
+#     return inch_value * 2.54
 
-print(inch_to_cm(inch))
+# print(inch_to_cm(inch))
+
+
+# Q7. Remove words from the list and strip it at the same?
+
+# def remove(list, word):
+#     n = []
+#     for item in list:
+#         if not (item == word):
+#             n.append(item.strip(word))
+#     return n
+
+# list = ["Harry", "Sachin", "Rohan", "an", "Alok"]
+
+# print(remove(list, "ry"))
+
+
+# Q8. Multiplication 
+
+def multiplication(n):
+    for i in range(1,11):
+     print(f"{n} X {i} = {n*i}")
+
+multiplication(5)
