0e0a7755235c73c6953762fc9e260d7b06ad9cd8
Sachin Singh <sachin8174056@gmail.com>
2025-12-24 22:24:16 +0530

    lambda

---
+++ Self/lambda_fun.py
@@ -0,0 +1,21 @@
+'''
+# 1. Lambda Function
+# Normal Function
+
+n = int(input("Enter a number :"))
+
+# def square(n):
+#     return n*n
+
+square = lambda n: n*n
+print(square(n))
+
+'''
+
+'''
+2. Lambda with multiple argument 
+
+add = lambda a, b : a+b
+print(add(3,4))
+
+'''
