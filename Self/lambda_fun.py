257b6ffa09a3de7c005578ee1102418b7c1985f8
Sachin Singh <sachin8174056@gmail.com>
2025-12-26 23:16:51 +0530

    lambda

--- Self/lambda_fun.py
+++ Self/lambda_fun.py
@@ -16,3 +16,24 @@ print(square(n))
 
 add = lambda a, b : a+b
 print(add(3,4))
+
+
+🔹 3. List Comprehension (Very Important)
+Normal Way
+even_numbers = []
+for num in [1, 2, 3, 4, 5, 6]:
+    if num % 2 == 0:
+        even_numbers.append(num)
+
+
+List Comprehension Way
+even_numbers = [num for num in [1, 2, 3, 4, 5, 6] if num % 2 == 0]
+print(even_numbers)
+
+
+
+🔹 4. List Comprehension with Strings
+names = ["Sachin", "Rohan", "Amit", "Sunil"]
+
+upper_names = [name.upper() for name in names]
+print(upper_names)
