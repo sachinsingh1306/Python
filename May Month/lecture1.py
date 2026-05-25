66898e5ddcab2fc3186672c1e386f52c32917427
SACHIN SINGH <sachin8174056@gmail.com>
2026-05-25 08:54:05 +0530

    update

---
+++ May Month/lecture1.py
@@ -0,0 +1,61 @@
+print("hello sachin !!!")
+
+# Programming Language -> Python
+# Machine <- translator (compiler/interpreter)---- python
+# What is Python ?
+
+# Python is simple and easy
+# Free & Open Source
+# High Level Language
+# Developer by Guido Van Rossum
+# Portable
+
+
+name = 'Sachin Singh'
+print(name)
+
+name = "My name is Sachin Singh","My age is 28year old"
+print(name)
+
+print(10+10)
+
+# Variables = A variable is a name given to a memory location in a program.
+
+name = 'Sachin Singh'
+age = "38year Old"
+luck_no = 123
+price = 55.99
+
+print(name,age,price)
+print("My name is",name)
+print(age)
+print(price)
+
+# Rules for identifier
+# Allowed == A to Z , a t0 z, 0 to 9 , (_)
+# Not allowed = !@#$% and do not start with the 0 To 9
+
+print(type(name))
+print(type(luck_no))
+print(type(age))
+print(type(price))
+
+# Data Types
+
+# Interger --> 123,4, -45, +45
+# String --> "sachin" , "hello buddy"
+# Float --> 455.77
+# Boolean --> true/false
+# None  
+
+age = 32
+old = False
+a = None
+
+print(type(age))
+print(type(old))
+print(type(a))
+
+# KeyWords are reserved words in python
+
+ 
